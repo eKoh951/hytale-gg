1043b765d2f8a9a91c88f8d71c47c4091f6fe5ea
eKoh951 <greatekoh@gmail.com>
2026-02-03 18:37:38 -0700

    update

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,12 +1,16 @@
+
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export async function createClient() {
   const cookieStore = await cookies();
 
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    supabaseUrl,
+    supabaseKey,
     {
       cookies: {
         getAll() {
@@ -14,8 +18,8 @@ export async function createClient() {
         },
         setAll(cookiesToSet) {
           try {
-            cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options),
+            cookiesToSet.forEach(({ name, value, options }) => 
+              cookieStore.set(name, value, options)
             );
           } catch {
             // The `setAll` method was called from a Server Component.
@@ -24,6 +28,6 @@ export async function createClient() {
           }
         },
       },
-    },
+    }
   );
 }
