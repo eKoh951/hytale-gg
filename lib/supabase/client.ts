dd481e96e88b5ce8458405d59d20f014ce3d2d78
eKoh951 <greatekoh@gmail.com>
2026-02-03 18:44:03 -0700

    fix

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,12 +1,8 @@
-
-import { createBrowserClient } from "@supabase/ssr";
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
   return createBrowserClient(
-    supabaseUrl,
-    supabaseKey
-  );
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+  )
 }
